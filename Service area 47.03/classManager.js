fa9887020257749a40edd0ff38dd6517f03e524f
pin84 <qp1984@live.com>
2024-01-22 11:08:13 +0800

    save

--- Service area 47.03/classManager.js
+++
@@ -1,9 +0,0 @@
-_partnerAmenity = null
-async function partnerAmenity() {
-  if (!_partnerAmenity) {
-    await svcUtils.loadJs(_path + '/partner/amenity.js');
-    svcUtils.loadCssList(['./partner/amenity.css'])
-    _partnerAmenity = new PartnerAmenity();
-  }
-  return _partnerAmenity;
-}
